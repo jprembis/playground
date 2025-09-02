528a1657e3ecfe8f410637bd41f2a9bd02911c05
Jakub Rembisz <jprembis@gmail.com>
2025-09-01 22:22:08 -0400

    Add more test cases

--- arith/src/lib.rs
+++ arith/src/lib.rs
@@ -1,6 +1,9 @@
 /// Returns the square root of a floating-point number `n` using the
 /// Newton-Raphson method.
 pub fn sqrt(n: f32) -> f32 {
+    if n < 0.0 {
+        return f32::NAN;
+    }
     let mut g = n;
     while (g - n / g).abs() > f32::EPSILON * g {
         g = (g + n / g) / 2.0;
@@ -33,13 +36,21 @@ mod tests {
     fn test_sqrt() {
         let checked_sqrt = |n: f32| {
             let root = sqrt(n);
-            let radicand = root * root;
-            assert!((n - radicand).abs() <= 2.0 * f32::EPSILON * n);
+            if root >= 0.0 {
+                let radicand = (root * root).clamp(0.0, f32::MAX);
+                assert!((n - radicand).abs() <= 2.0 * f32::EPSILON * n);
+            }
         };
         let mut rng = rand::rng();
+        checked_sqrt(f32::NAN);
+        checked_sqrt(f32::NEG_INFINITY);
+        checked_sqrt(-1.0);
+        checked_sqrt(0.0);
         for _ in 0..1000 {
             checked_sqrt(rng.random_range(0.0..f32::MAX));
         }
+        checked_sqrt(f32::MAX);
+        checked_sqrt(f32::INFINITY);
     }
 
     #[test]
